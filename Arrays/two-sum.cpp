ed84277d4c73363e1145bf02632c23a2522b1b7e
Unnati-29 <rajunnati6@gmail.com>
2026-05-24 06:07:32 +0530

    Update two-sum.cpp

--- Arrays/two-sum.cpp
+++ Arrays/two-sum.cpp
@@ -1,1 +1,25 @@
+// LeetCode 1 - Two Sum
+// Topic: Arrays + HashMap
+// Difficulty: Easy
+// Time Complexity: O(n)
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<nums.size();i++){
+
+            int needed = target - nums[i];
+
+            if(mp.find(needed) != mp.end()){
+                return {mp[needed], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
