35b315afc29aaa9df80e5929906be1d6543a8ba1
Unnati-29 <rajunnati6@gmail.com>
2026-06-11 20:13:43 +0530

    Create Number of Students Unable to Eat Lunch.cpp

---
+++ Stacks -and-Queue/Number of Students Unable to Eat Lunch.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+
+        queue<int> q;
+
+        for (int x : students) {
+            q.push(x);
+        }
+
+        int i = 0,rotations = 0;     
+
+        while (!q.empty() && rotations < q.size()) {
+
+            if (q.front() == sandwiches[i]) {
+                q.pop();
+                i++;
+                rotations = 0;  
+            }
+            else {
+                q.push(q.front());
+                q.pop();
+                rotations++;
+            }
+        }
+
+        return q.size();
+    }
+};
