8da5dd6ccabab5b04624ddaa24627bee78257073
Unnati-29 <rajunnati6@gmail.com>
2026-06-16 22:29:33 +0530

    Create Balanced Binary Tree.cpp

---
+++ Binary Tree/Balanced Binary Tree.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(root == NULL){
+            return 0;
+        }
+
+        int left = height(root->left);
+        int right = height(root->right);
+
+        return 1+max(left,right);
+    }
+
+    bool isBalanced(TreeNode* root) {
+        
+        if(root == NULL){
+            return true;
+        }
+
+        int left = height(root->left);
+        int right = height(root->right);
+
+        if(abs(left - right) > 1) {
+            return false;
+        }
+        
+         return isBalanced(root->left) && isBalanced(root->right); 
+    }
+};
