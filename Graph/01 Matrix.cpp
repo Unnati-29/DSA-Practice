346bb163e9cac5c6630cab4a10b56cad39b8ea2a
Unnati-29 <rajunnati6@gmail.com>
2026-07-23 07:57:49 +0530

    Create 01 Matrix.cpp

---
+++ Graph/01 Matrix.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        queue<pair<int,int>> q;
+
+        vector<vector<int>> dist(m, vector<int>(n, -1));
+
+        for(int i = 0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j] == 0){
+                    q.push({i, j});
+                    dist[i][j] = 0;
+                }
+            }
+        }
+
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
+
+        while(!q.empty()){
+            auto curr = q.front();
+            q.pop();
+
+            int x = curr.first;
+            int y = curr.second;
+
+            for(int k = 0; k < 4; k++) {
+
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                // Check boundaries
+                if(nx >= 0 && nx < m && ny >= 0 && ny < n &&
+                   dist[nx][ny] == -1) {
+
+                    dist[nx][ny] = dist[x][y] + 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+        return dist;
+    }
+};
